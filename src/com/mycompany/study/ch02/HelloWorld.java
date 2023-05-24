8798c1ad4853bc950849c6244449d4b14a5de3c0
SlowGlowRoot <gofla7956@naver.com>
2023-05-24 16:12:19 +0900

    error

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World.");
+		System.out.println("제 이름은 서희근입니다.");
+		System.out.println("올해로 20세입니다.");
+		System.out.println("거주지는 경기도 김포시입니다.");
+		System.out.println("취미는 프로그래밍입니다.");
+		System.out.println("잘 부탁드립니다.");
+		System.out.println("");
+		System.out.println("컴파일 키는 Ctrl + F11키 입니다.");
+		
+	}
+
+}
