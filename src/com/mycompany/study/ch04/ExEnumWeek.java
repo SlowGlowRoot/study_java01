4187b3700ac964d48f146533cfdcb72d4e75975c
SlowGlowRoot <gofla7956@naver.com>
2023-06-05 15:32:17 +0900

    commit

--- src/com/mycompany/study/ch04/ExEnumWeek.java
+++ src/com/mycompany/study/ch04/ExEnumWeek.java
@@ -4,9 +4,16 @@ public class ExEnumWeek {
 	
 	public static void main(String[] args) {
 		
+							// DataType과 int형, EnumType과 Week의 관계가 동일함.
 		int a = 1;
+		String abc;
+//		abc = Week.MONDAY;	// err: Type Mismatch
+		abc = Week.MONDAY.toString();	// .toString()을 뒤에 붙여주면 Monday를 강제로 String형으로 변환한다. -> "MONDAY"
+		System.out.println(abc);
+		
 		Week week;
-		// DataType과 int형, EnumType과 Week와의 관계가 동일함.
+		week = Week.MONDAY;
+		System.out.println(week);
 		
 	}
 
